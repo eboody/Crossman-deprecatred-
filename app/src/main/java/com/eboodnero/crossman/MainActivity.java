c2ea408d876e7d9327cc1d9b84352f2d61ca44d7
Eran Boodnero <eboodnero@gmail.com>
2015-06-16 23:13:21 -0700

    add ability to add more fragments to MainActivity and scroll

--- app/src/main/java/com/eboodnero/crossman/MainActivity.java
+++ app/src/main/java/com/eboodnero/crossman/MainActivity.java
@@ -1,16 +1,15 @@
 package com.eboodnero.crossman;
 
 import android.net.Uri;
-import android.support.v4.widget.DrawerLayout;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.AttributeSet;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
-implements hiit_timer.OnFragmentInteractionListener{
+implements HiitTimer.OnFragmentInteractionListener{
 
     private Toolbar toolbar;
 
@@ -19,8 +18,33 @@ implements hiit_timer.OnFragmentInteractionListener{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /*set our toolbar to act like an actionbar*/
         toolbar = (Toolbar)findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
+
+        if (findViewById(R.id.fragment_container_1) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            // Create a new Fragment to be placed in the activity layout
+            HiitTimer firstFragment = new HiitTimer();
+            HiitTimer secondFragment = new HiitTimer();
+
+            // In case this activity was started with special instructions from an
+            // Intent, pass the Intent's extras to the fragment as arguments
+            firstFragment.setArguments(getIntent().getExtras());
+
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container_1, firstFragment).commit();
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container_2, secondFragment).commit();
+
+        }
 
     }
 
